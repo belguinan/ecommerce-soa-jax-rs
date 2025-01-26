7e475915ba7bd50e3fcfabeec73b131ef1d6bdb5
Noureddine Belguinan <nor15dine@gmail.com>
2025-01-26 14:14:09 +0100

    wip

--- userservice/src/main/java/com/example/userservice/contracts/UserServiceContract.java
+++ userservice/src/main/java/com/example/userservice/contracts/UserServiceContract.java
@@ -7,13 +7,31 @@ import java.util.Optional;
 
 public interface UserServiceContract {
 
+    /**
+     * @return User
+     */
     public User show();
 
+    /**
+     * @param user
+     * @return User
+     */
     public User register(User user);
 
+    /**
+     * @param loginRequest
+     * @return Optional<String>
+     */
     public Optional<String> login(LoginRequest loginRequest);
 
+    /**
+     * @return boolean
+     */
     public boolean logout();
 
+    /**
+     * @param user
+     * @return User
+     */
     public User update(User user);
 }
